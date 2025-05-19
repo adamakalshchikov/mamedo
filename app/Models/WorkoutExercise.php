c806e8393f6cd4dce4cc6f479d73a57fe96e2635
Alex Damakalshchikov <a.damakalshchikov@gmail.com>
2025-05-19 22:31:31 +0200

    add WorkoutExercise

--- app/Models/WorkoutExercise.php
+++ app/Models/WorkoutExercise.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class WorkoutExercise extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'workout_id',
+        'exercise_id',
+        'sets_data',
+        'notes'
+    ];
+
+    public function workout(): BelongsTo
+    {
+        return $this->belongsTo(Workout::class);
+    }
+
+    public function excercise():BelongsTo
+    {
+        return $this->belongsTo(Exercise::class);
+    }
 }
