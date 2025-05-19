3fa37a4ddeca25083a4c2fd890d5779d6a3890a7
Alex Damakalshchikov <a.damakalshchikov@gmail.com>
2025-05-19 22:04:18 +0200

    upd workout

--- app/Models/Workout.php
+++ app/Models/Workout.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\DB;
 
 class Workout extends Model
 {
@@ -20,12 +22,31 @@ class Workout extends Model
 
     public function setDate(Carbon|string $date): self
     {
-        if (is_string($date)){
+        if (is_string($date)) {
             $this->date = Carbon::parse($date);
+        } else {
+            $this->date = $date;
         }
-        $this->date = $date;
         return $this;
     }
 
-    //
+    public function workoutExcersises(): HasMany
+    {
+        return $this->hasMany(WorkoutExercise::class);
+    }
+
+    public static function createWorkoutWithExcersis(Carbon|string $date, array $exercises): self
+    {
+        DB::beginTransaction();
+        try {
+            $workout = self::create(['date' => $date]);
+            foreach ($exercises as $exercise) {
+                $workout->workoutExcersises()->create($exercise);
+            }
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+        return $workout;
+    }
 }
